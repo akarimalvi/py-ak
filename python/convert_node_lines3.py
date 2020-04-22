9428e6df6b5a5fd83321ea836a69bfee7761af4f
Akarim <asad.karim.alvi@gmail.com>
2020-04-22 10:53:19 +0500

    added files convert_node_lines3.py and convert_node_lines3.py

---
+++ python/convert_node_lines3.py
@@ -0,0 +1,26 @@
+# Import modules
+import sys
+
+file = open(sys.argv[1])
+lines = file.readlines()
+
+
+
+# Read the date given in argument
+##date = sys.argv[2]
+
+## Loop on all lines
+
+f = lines.split()[0] == "LOCUS"
+print(f)
+		##splitted_line = line.split()
+		##print(splitted_line)
+		
+		
+		
+		
+		#print(splitted_line[0] + '        ' + str(splitted_line[1].split('_')[0]) + '_' + str(splitted_line[1].split('_')[1]) + "            " + str(splitted_line[1].split('_')[3]) + " bp    DNA     linear       " + date)
+	#elif (date in line):
+		#continue
+    #else:
+		#print(line.replace('\n', '')) 
