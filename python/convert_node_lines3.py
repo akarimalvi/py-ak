78759dfc80a9c5e2cb9fdfeea51698dd5d6798b8
Akarim <asad.karim.alvi@gmail.com>
2020-04-22 15:49:38 +0500

    updated KJ473812.1.gbk and convert_node_lines3.py

--- python/convert_node_lines3.py
+++ python/convert_node_lines3.py
@@ -1,26 +1,22 @@
 # Import modules
 import sys
+# Read the given file
 
-file = open(sys.argv[1])
-lines = file.readlines()
+with open(sys.argv[1]) as filename:
+    content = filename.readlines()
 
-
-asad
 # Read the date given in argument
-##date = sys.argv[2]
+date = sys.argv[2]
+hunza = sys.argv[3]
 
 ## Loop on all lines
-
-f = lines.split()[0] == "LOCUS"
-print(f)
-		##splitted_line = line.split()
-		##print(splitted_line)
-		
-		
-		
-		
-		#print(splitted_line[0] + '        ' + str(splitted_line[1].split('_')[0]) + '_' + str(splitted_line[1].split('_')[1]) + "            " + str(splitted_line[1].split('_')[3]) + " bp    DNA     linear       " + date)
-	#elif (date in line):
-		#continue
-    #else:
-		#print(line.replace('\n', '')) 
+for f in content:
+    f.split()[0] == "LOCUS"
+    splitted_line = f.split()
+    
+    #print(splitted_line[0]     + splitted_line[1])
+    
+    #print(splitted_line[0] + ' ' + str(splitted_line[1].split('_')[0]) +' '+ str(splitted_line[1].split('_')[1]) + ' '+ str(splitted_line[1].split('_')[2]) +' '+ str(splitted_line[2]) + ' ' + date)
+    
+    print(splitted_line[0] + ' '+ str(splitted_line[1].replace('asad', hunza).split('_')[0])+' '+ str(splitted_line[2]) + ' '+ str(splitted_line[3])+ ' ' + date)
+    
